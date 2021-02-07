69c275eca203d2146933080e063788924212576b
knewjade <knewjade@users.noreply.github.com>
2021-02-07 09:15:08 +0000

    deploy: knewjade/solution-finder@0e727482c5d5877f76703a059405f275ce2e6eb8

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.950',
+    VERSION: '0.960',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
